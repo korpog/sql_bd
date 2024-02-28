06dd5766539f5169e8146780973fbed91ec852b8
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-02-28 20:56:09 +0100

    updated tables

--- projekt.sql
+++ projekt.sql
@@ -6,21 +6,30 @@ CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL
-    email VARCHAR(100) NOT NULL
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE INDEX idx_name_surname
+ON users (name, surname);
+
 CREATE TABLE posts (
     post_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
     title VARCHAR(100) NOT NULL,
     body TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+CREATE INDEX idx_title
+ON posts (title);
+
 CREATE TABLE comments (
-    post_id INT PRIMARY KEY AUTO_INCREMENT,
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    post_id INT,
     body TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(user_id),
@@ -28,14 +37,18 @@ CREATE TABLE comments (
 );
 
 CREATE TABLE likes (
-    post_id INT PRIMARY KEY AUTO_INCREMENT,
+    like_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    post_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(user_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE followers (
+    user1_id INT,
+    user2_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user1_id) REFERENCES users(user_id),
+    FOREIGN KEY (user2_id) REFERENCES users(user_id)
 );
