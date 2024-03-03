95db8438aa534949f4597ac91248c8059e6a3d3d
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-03-03 08:10:43 +0100

    more data

--- projekt.sql
+++ projekt.sql
@@ -18,7 +18,12 @@ VALUES
 ('Dale', 'Cooper', 'coop', 35, 'dc@tp.net'),
 ('Bobby', 'Briggs', 'bobby', 19, 'bb@tp.net'),
 ('Shelly', 'Johnson', 'cherrypie', 19, 'sj@tp.net'),
-('Leland', 'Palmer', 'bob', 45, 'bob@tp.net');
+('Leland', 'Palmer', 'bob', 45, 'bob@tp.net'),
+('Audrey', 'Horne', 'audrey', 18, 'audrey@tp.net'),
+('Donna', 'Hayward', 'donna', 17, 'donna@tp.net'),
+('Madeleine', 'Ferguson', 'maddie', 20, 'maddie@tp.net'),
+('Pete', 'Martell', 'pete', 52, 'pete@tp.net'),
+('James', 'Hurley', 'james', 18, 'james@tp.net');
 
 CREATE INDEX idx_name_surname
 ON users (name, surname);
@@ -35,6 +40,8 @@ CREATE TABLE posts (
 INSERT INTO posts(user_id, title, body)
 VALUES
 (1, 'Dear Diary', 'Tonight is the night that I die'),
+(1, 'Faster and faster', "And for a long time you wouldn't feel anything. And then you'd burst into fire.
+ Forever... And the angels wouldn't help you. Because they've all gone away."),
 (2, 'Damn fine coffee', 'You know, this is - excuse me - a damn fine cup of coffee.'),
 (3, 'You want to know who killed Laura? You did! We all did.', "She said people try to be good but they're really sick and rotten, her most of all"),
 (4, 'Leo doesn’t talk, he hits', 'He was so great at first, you know. This flashy guy in his hot car.'),
@@ -56,7 +63,9 @@ CREATE TABLE comments (
 INSERT INTO comments(user_id, post_id, body)
 VALUES
 (1, 3, "I was standing right behind you, but you're too dumb to turn around."),
-(2, 5, "The time has come for you to seek the Path.");
+(2, 5, "The time has come for you to seek the Path."),
+(7, 1, "As much as I love you, Laura, most of the time we were trying to solve your problems."),
+(9, 1, "She's dead. Wrapped in plastic.");
 
 CREATE TABLE likes (
     like_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -98,7 +107,20 @@ WHERE title LIKE 'D%';
 SELECT SUM(age) from users;
 
 SELECT name, surname FROM users
-WHERE age BETWEEN 20 AND 40;
+WHERE age BETWEEN 20 AND 40
+ORDER BY NAME;
+
+
+
+SELECT p.post_id, p.title, u.username, COUNT(*) AS num_of_likes 
+FROM posts AS p
+INNER JOIN likes AS l ON p.post_id = l.post_id
+INNER JOIN users AS u ON u.user_id = p.user_id
+GROUP BY l.post_id
+ORDER BY num_of_likes DESC;
+
+
+
 
 
 
