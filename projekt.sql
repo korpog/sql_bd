9f33390bc82adaefc9f842f30fc44c7e7a7f505c
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-03-06 22:59:32 +0100

    more data

--- projekt.sql
+++ projekt.sql
@@ -45,7 +45,12 @@ VALUES
 (2, 'Damn fine coffee', 'You know, this is - excuse me - a damn fine cup of coffee.'),
 (3, 'You want to know who killed Laura? You did! We all did.', "She said people try to be good but they're really sick and rotten, her most of all"),
 (4, 'Leo doesn’t talk, he hits', 'He was so great at first, you know. This flashy guy in his hot car.'),
-(5, "You may think I've gone insane", "... but I promise. I will kill again.");
+(5, "You may think I've gone insane", "... but I promise. I will kill again."),
+(5, "I was just a boy. I saw him in my dreams.", "When he was inside, I didn’t know and when he was gone I couldn’t remember. He made me do things, terrible things. He said he wanted lives, he wanted others."),
+(9, "Fellas, don't drink that coffee!", "You'd never guess. There was a fish in the percolator! Sorry."),
+(8, "When we were growing up, Laura and I were so close.", "It was scary. I could feel her thoughts, like our brains were connected or something."),
+(10, "I remember this one night when we first started seeing each other", "She was still doing drugs then. Well, we were in the woods when she started saying this scary poem, over and over, about fire.");
+
 
 CREATE INDEX idx_title
 ON posts (title);
@@ -65,7 +70,14 @@ VALUES
 (1, 3, "I was standing right behind you, but you're too dumb to turn around."),
 (2, 5, "The time has come for you to seek the Path."),
 (7, 1, "As much as I love you, Laura, most of the time we were trying to solve your problems."),
-(9, 1, "She's dead. Wrapped in plastic.");
+(9, 1, "She's dead. Wrapped in plastic."),
+(1, 1, "I am dead. Yet I live."),
+(8, 10, "You looked at me and you saw Laura."),
+(10, 2, "You're not a turkey. A turkey is one of the dumbest birds on earth."),
+(2, 3, "Diane, 11:30 a.m., February Twenty-fourth. Entering the town of Twin Peaks, five miles south of the Canadian border, twelve miles west of the state line."),
+(2, 4, "You didn't love her anyway."),
+(1, 2, "I feel like I know her... but sometimes my arms bend back.");
+
 
 CREATE TABLE likes (
     like_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -77,7 +89,7 @@ CREATE TABLE likes (
 );
 
 INSERT INTO likes(user_id, post_id)
-VALUES (1, 3), (1,2), (5,5), (5,1), (2,4), (4,2);
+VALUES (1,3), (1,2), (5,5), (5,1), (2,4), (4,2), (7,10), (4,6), (7,1), (1,9);
 
 CREATE TABLE follows (
     following_user_id INT,
@@ -88,7 +100,7 @@ CREATE TABLE follows (
 );
 
 INSERT INTO follows(following_user_id, followed_user_id)
-VALUES (3,1), (3,4), (2,1), (5,1);
+VALUES (3,1), (3,4), (2,1), (5,1), (10, 1), (8,1), (7,1), (5,8), (6,2), (10,8), (8,10);
 
 
 
